55d0e2f1e6bc7b82a077ac3bbca06ce0d61b5c6c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-13 16:23:02 +0200

    feat: queue cors, page functional

--- api/src/routes/queue.ts
+++ api/src/routes/queue.ts
@@ -2,10 +2,12 @@ import { Hono } from "hono";
 import get from "./queue/get";
 import post from "./queue/post";
 import middleware from "./queue/middleware";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.use(middleware);
+app.use(cors());
 
 app.get("/", get);
 app.post("/", post);
