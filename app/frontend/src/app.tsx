541589cb46498e77013b17cc69ec1460eb4346e5
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-17 17:12:09 +0200

    feat(app): tailwind

--- app/frontend/src/app.tsx
+++ app/frontend/src/app.tsx
@@ -1,30 +1,43 @@
-import './App.css'
-import logo from "./assets/images/logo-universal.png"
-import {Greet} from "../wailsjs/go/main/App";
-import {useState} from "preact/hooks";
-import {h} from 'preact';
+import "./App.css";
+import logo from "./assets/images/logo-universal.png";
+import { Greet } from "../wailsjs/go/main/App";
+import { useState } from "preact/hooks";
+import { h } from "preact";
 
 export function App(props: any) {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
 
-    return (
-        <>
-            <div id="App">
-                <img src={logo} id="logo" alt="logo"/>
-                <div id="result" className="result">{resultText}</div>
-                <div id="input" className="input-box">
-                    <input id="name" className="input" onChange={updateName} autoComplete="off" name="input"
-                           type="text"/>
-                    <button className="btn" onClick={greet}>Greet</button>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div id="App">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <img src={logo} id="logo" alt="logo" />
+        <div id="result" className="result">
+          {resultText}
+        </div>
+        <div id="input" className="input-box">
+          <input
+            id="name"
+            className="input"
+            onChange={updateName}
+            autoComplete="off"
+            name="input"
+            type="text"
+          />
+          <button className="btn" onClick={greet}>
+            Greet
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
