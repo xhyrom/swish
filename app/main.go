ed9df041e56390cef04902fda2bbb67154e64212
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-17 21:25:14 +0200

    fix(app): probably db issue fixed

--- app/main.go
+++ app/main.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"embed"
 	"fmt"
-	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -25,9 +24,10 @@ func main() {
 	// db connection
 	creds := database.GetCredentials()
 	db := database.NewDatabase(creds)
+	ctx := context.Background()
 	defer db.Close()
 
-	go listener(app, db)
+	go listen(app, ctx, db)
 
 	// Create application with options
 	err := wails.Run(&options.App{
@@ -45,69 +45,32 @@ func main() {
 		},
 	})
 
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
 }
 
+func listen(app *App, ctx context.Context, db *database.Database) {
+	for {
+		con, err := db.Acquire(ctx)
+		if err != nil {
+			fmt.Println("Error acquiring connection:", err)
+			return
+		}
 
-func establishConnectionAndListen(app *App, db *database.Database) (*pgxpool.Conn, error) {
-	conn, err := db.Acquire(context.Background())
-	if err != nil {
-		return nil, fmt.Errorf("Error acquiring connection: %w", err)
+		con.Conn().Exec(ctx, "LISTEN table_changes")
+
+		n, _ := con.Conn().WaitForNotification(ctx)
+
+		go func(n *pgconn.Notification) {
+			poolCon, _ := db.Acquire(ctx)
+			handleNotification(app, n)
+			poolCon.Release()
+		}(n)
 	}
-
-	_, err = conn.Exec(context.Background(), "LISTEN table_changes")
-	if err != nil {
-		return nil, fmt.Errorf("Error listen to table_changes: %w", err)
-	}
-
-	return conn, nil
 }
 
-
-func listener(app *App, db *database.Database) {
-	conn, err := establishConnectionAndListen(app, db)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Start a goroutine to ping the database periodically
-	go func() {
-		for {
-			// Acquire a new connection for pinging
-			pingConn, err := db.Acquire(context.Background())
-			if err != nil {
-				fmt.Println("Error acquiring connection for ping:", err)
-				continue
-			}
-
-			err = pingConn.Ping(context.Background())
-			pingConn.Release() // Don't forget to release the connection when you're done
-
-			if err != nil {
-				fmt.Println("Lost connection to the database, reconnecting...")
-				conn, err = establishConnectionAndListen(app, db)
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println("Reconnected to the database.")
-				}
-			}
-			time.Sleep(time.Minute) // Adjust the sleep duration to your needs
-		}
-	}()
-
-	for {
-		notification, err := conn.Conn().WaitForNotification(context.Background())
-		if err != nil {
-			fmt.Println("Error waiting for notification:", err)
-			continue
-		}
-
-		fmt.Println("Received notification:", notification)
-		runtime.EventsEmit(app.ctx, "table_changes", notification.Payload)
-	}
+func handleNotification(app *App, notification *pgconn.Notification) {
+	fmt.Println("Received notification:", notification)
+	runtime.EventsEmit(app.ctx, "table_changes", notification.Payload)
 }
